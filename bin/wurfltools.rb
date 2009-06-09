7144ef26fb20ea45756ef4b55a17c185024132fc
Paul McMahon <paul@mobalean.com>
2009-06-09 15:21:10 +0900

    Add list of available commands

--- bin/wurfltools.rb
+++ bin/wurfltools.rb
@@ -4,6 +4,10 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..',  'lib')
 
 def usage
   puts "usage: wurfltools.rb command options"
+  path = "#{File.dirname(__FILE__)}/../lib/wurfl/command/*"
+  a = Dir.glob(path).map {|s| File.basename(s,".rb")}
+  puts "available commands:"
+  puts a
   exit
 end
 
