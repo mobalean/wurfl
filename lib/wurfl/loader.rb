4bbac8ca790d147c04b6f3df33c53af309b6f0da
Paul McMahon <paul@mobalean.com>
2009-07-22 18:29:24 +0900

    Convert tabs to spaces

--- lib/wurfl/loader.rb
+++ lib/wurfl/loader.rb
@@ -44,11 +44,10 @@ class Wurfl::Loader
 
   # Prints out WURFL handsets from a hashtable
   def print_wurfl(handsets)
-    
     handsets.each do |key,value|
       puts "********************************************\n\n"
       puts "#{key}\n"
-      value.each { |key,value| 	puts "#{key} = #{value}" }
+      value.each { |key,value|   puts "#{key} = #{value}" }
     end
   end
 
@@ -60,48 +59,46 @@ class Wurfl::Loader
     
     # iterate over all of the devices in the file
     doc.find("///devices/device").each do |element| 
-      
       rcount += 1
       hands = nil # the reference to the current handset
       if element.attributes["id"] == "generic"
-	# setup the generic Handset 
-	
-	if @handsets.key?("generic") then
-	  hands = @handsets["generic"]
-	else
-	  # the generic handset has not been created.  Make it
-	  hands = Wurfl::Handset.new "generic","generic"
-	  @handsets["generic"] = hands
-	end
-	
+        # setup the generic Handset 
+        
+        if @handsets.key?("generic") then
+          hands = @handsets["generic"]
+        else
+          # the generic handset has not been created.  Make it
+          hands = Wurfl::Handset.new "generic","generic"
+          @handsets["generic"] = hands
+        end
       else
-	# Setup an actual handset
-	
-	# check if handset already exists.
-	wurflid = element.attributes["id"]	
-	if @handsets.key?(wurflid)
-	  # Must have been created by someone who named it as a fallback earlier.
-	  hands = @handsets[wurflid]
-	else
-	  hands = Wurfl::Handset.new "",""
-	end
-	hands.wurfl_id = wurflid
-	hands.user_agent = element.attributes["user_agent"]
-	
-	# get the fallback and copy it's values into this handset's hashtable
-	fallb = element.attributes["fall_back"]
-	
-	# Now set the handset to the proper fallback reference
-	if !@handsets.key?(fallb)
-	  # We have a fallback that does not exist yet, create the reference.
-	  @handsets[fallb] = Wurfl::Handset.new "",""
-	end
-	hands.fallback = @handsets[fallb]
+        # Setup an actual handset
+        
+        # check if handset already exists.
+        wurflid = element.attributes["id"]  
+        if @handsets.key?(wurflid)
+          # Must have been created by someone who named it as a fallback earlier.
+          hands = @handsets[wurflid]
+        else
+          hands = Wurfl::Handset.new "",""
+        end
+        hands.wurfl_id = wurflid
+        hands.user_agent = element.attributes["user_agent"]
+        
+        # get the fallback and copy it's values into this handset's hashtable
+        fallb = element.attributes["fall_back"]
+        
+        # Now set the handset to the proper fallback reference
+        if !@handsets.key?(fallb)
+          # We have a fallback that does not exist yet, create the reference.
+          @handsets[fallb] = Wurfl::Handset.new "",""
+        end
+        hands.fallback = @handsets[fallb]
       end
       
       # now copy this handset's specific capabilities into it's hashtable
       element.find("group/capability").each do |el2|
-	hands[el2.attributes["name"]] = el2.attributes["value"]
+        hands[el2.attributes["name"]] = el2.attributes["value"]
       end
       @handsets[hands.wurfl_id] = hands
       
