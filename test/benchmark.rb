87fe6e9ca25f9bf6152a0c342f1af2bd99bd44cc
Paul McMahon <paul@mobalean.com>
2010-02-11 15:29:32 +0900

    Fix so that benchmark isn't run when required

--- test/benchmark.rb
+++ test/benchmark.rb
@@ -2,8 +2,10 @@ $LOAD_PATH << File.join(File.dirname(__FILE__), '..', 'lib')
 require 'wurfl/loader'
 require 'benchmark'
 
-loader = Wurfl::Loader.new
-r = Benchmark.measure do
-  loader.load_wurfl(File.join(File.dirname(__FILE__), "data", "wurfl.large.xml"))
+if $0 == __FILE__
+  loader = Wurfl::Loader.new
+  r = Benchmark.measure do
+    loader.load_wurfl(File.join(File.dirname(__FILE__), "data", "wurfl.large.xml"))
+  end
+  puts r
 end
-puts r
